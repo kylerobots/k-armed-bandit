67973293c00f9c586001b7c4f12105eb7b17ff49
Kyle Hart <23261132+kylerobots@users.noreply.github.com>
2021-01-22 19:48:11 -0500

    Cleaned up and elaborated on Static test

--- bandit/test_bandit.py
+++ bandit/test_bandit.py
@@ -1,5 +1,4 @@
-from numpy.lib.function_base import kaiser
-from bandit.base import Bandit
+from bandit.Base import Bandit
 import unittest
 from bandit.static import Static
 import numpy
@@ -10,69 +9,81 @@ class TestStaticBandit(unittest.TestCase):
     Test case to test that the static bandit works correctly.
     """
 
-    def test_instantiateK(self):
+    def test_instantiate_k(self):
         """
-        Test that the Static bandit can handle various values of k
-        correctly.
+        Test that the Static bandit properly sets the number of arms. Any
+        positive integer is permitted and everything else is rejected. This
+        mimics real life where you can't have a non-natural number of arms.
+        Zero is explicitly excluded, since it makes the object trivial.
         """
         # These values should all work
         for k in (1, 2, 100):
             bandit = Static(k, None)
-            self.assertIsNotNone(bandit)
-        # k can't be changed once set
-        with self.assertRaises(AttributeError):
-            bandit = Static(3, None)
-            bandit.k = 5
+            self.assertEqual(
+                bandit.k, k, 'Static bandit did not create the correct number of arms.')
         # These should all fail
         for k in (0, -1, 0.5, '1', 'the', None):
-            # with self.subTest(k=k):
-            with self.assertRaises(ValueError):
+            with self.assertRaises(ValueError, 'Static bandit did not reject invalid k input.'):
                 Static(k, None)
 
-    def test_instantiateRewards(self):
+    def test_instantiate_rewards(self):
         """
-        Test that the class can handle reward values.
-
-        What values are acceptable somewhat depend on k, so there are several
-        tests to check.
+        Test that the class can handle reward values. Acceptable values should
+        be some sort of iterable with a number of elements equal to k. Each
+        element should be numeric. Alternatively, None can be used to have the
+        class randomly select values.
         """
-        # Test that it requires an iterable of the right length.
         k = 3
-        # These are all valid ways of defining the values
-        for values in ((1, 2, 3), [1, 2, 3], numpy.array([1, 2, 3]), None):
+        # Iterables can include lists, arrays, or numpy arrays, for starters.
+        for values in ((1, 2, 3), [1, 2, 3], numpy.array([1, 2, 3])):
             with self.subTest(values=values):
                 bandit = Static(k, values)
+                #  Make sure that the stored rewards are what was provided.
+                rewards = bandit.rewards
+                for i, value in enumerate(values):
+                    self.assertEqual(
+                        value, rewards[i], 'Stored reward does not match provided.')
                 self.assertIsNotNone(bandit)
-        # Once defined, you can't change the rewards
-        with self.assertRaises(AttributeError):
-            bandit = Static(3, None)
-            bandit.rewards = (1, 2, 3)
-        # These are all invalid ways
-        for values in ((1, 2), ('the', 'it', 4), 4, '1 2 3'):
-            with self.assertRaises(ValueError):
-                Static(k, values)
-        # Test that if none is provided, the resulting assigned values are in
-        # the right range. Since this is nondeterministic, check this a few
-        # times.
+        # The user can pass in None to randomly select rewards between [0, 1).
+        # Since this is technically random, try several times.
         for i in range(50):
             bandit = Static(k, None)
             values = bandit.trueValues()
             for value in values:
                 self.assertGreaterEqual(value, 0)
                 self.assertLess(value, 1)
+        # An iterable must have the same length as k, otherwise it should fail.
+        with self.assertRaises(ValueError, msg='Static bandit did not reject incorrect length rewards.'):
+            Static(k=3, rewards=(1, 2))
+        # Other, non-numeric iterables and data types should produce some form
+        # of error.
+        for values in ((1, 2), ('the', 'it', 4), 4, '1 2 3'):
+            with self.assertRaises(Exception, msg='Static bandit did not reject non-numeric rewards'):
+                Static(k, values)
 
     def test_correctRewards(self):
         """
-        Test that the right reward is returned when each arm is selected.
+        Test that the right reward is returned when each arm is selected. This
+        should allow any indexing inputs that you could use for numpy arrays
+        and reject everything else. The correct inputs should return the
+        appropriate rewards that match the reward values set by the class.
         """
-        bandit = Static(3, None)
-        rewards = bandit.trueValues()
-        # Each reward should match
-        for i in range(bandit.k):
-            reward = bandit.select(i)
-            self.assertEqual(reward, rewards[i])
-        # Additionally, incorrect inputs should be rejected.
-        for i in (-1, 3, 4, 0.5, '1', None, (1, 2, 3)):
+        k = 10
+        true_rewards = numpy.random.uniform(low=0, high=1, size=10)
+        bandit = Static(k, true_rewards)
+        # This method can accept a single integer.
+        for arm in (-3, 0, 2, 9):
+            expected_reward = bandit.select(arm)
+            self.assertEqual(
+                expected_reward, true_rewards[arm], 'Static bandit did not provide a correct reward.')
+        # It can also accept an iterable type.
+        arms = range(k)
+        expected_rewards = bandit.select(arms)
+        self.assertTrue(numpy.array_equal(
+            true_rewards[arms], expected_rewards))
+        # Additionally, incorrect inputs should be rejected, including indices
+        # out of range.
+        for i in (0.5, 10, '1', 'the', None):
             rewards = bandit.trueValues()
             with self.assertRaises(ValueError):
                 reward = bandit.select(i)
