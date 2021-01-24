1b63bab3499af61047a4dc5daefff3a136645445
Kyle Hart <23261132+kylerobots@users.noreply.github.com>
2021-01-24 09:08:21 -0500

    Create tests and class framework

---
+++ agent/epsilon_greedy.py
@@ -0,0 +1,43 @@
+from agent import BaseAgent
+
+
+class EpsilonGreedy(BaseAgent):
+    """
+    A greedy agent that occasionally explores.
+
+    This agent will primarily exploit when deciding its actions. However, it will occasionally choose to explore at a
+    rate of epsilon, which is provided at initialization. This gives it a chance to see if other actions are better
+    options.
+    """
+
+    def __init__(self, k: int, epsilon: float, start_value: float = 0.0) -> None:
+        """
+        Construct the agent.
+
+        @param k The number of actions to consider. This must be an int greater than zero.
+        @param epsilon The rate at which actions should randomly explore. As this is a probability, it should be between
+        0 and 1.
+        @param start_value The initial value to use in the table. All actions start with the same value.
+        @raise ValueError if epsilon is not a valid probability (between 0 and 1).
+        """
+        super().__init__(k, start_value=start_value)
+
+    def act(self) -> int:
+        """
+        Determine which action to take.
+
+        This will explore randomly over the actions at a rate of epsilon and inversely will exploit based on table
+        values at a rate of (1.0 - epsilon).
+        @return The index of the selected action to take. Gauranteed to be an int on the range [0, k).
+        """
+        pass
+
+    def update(self, action: int, reward: float) -> None:
+        """
+        Update the Q-table based on the last action.
+
+        This will use an incremental formulation of the mean of all rewards obtained so far as the values of the table.
+        @param action An index representing which action on the table was selected. It must be between [0, k).
+        @param reward The reward obtained from this action.
+        """
+        pass
